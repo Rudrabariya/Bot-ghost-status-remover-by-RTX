5b78960ada589574ca56e3c2fc6f77e8f058499b
Rudrabariya <154620525+Rudrabariya@users.noreply.github.com>
2024-01-15 21:26:14 +0000

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["👑 I am RTX","Spotify","Youtube","SUBSCRIBE❤️","THANKS 🌇"];
+const statusMessages = ["Join flyglobal today!", "Operations on Halt"];
 
 
 let currentIndex = 0;
